6dc2994d3dffd6b7784d3522757f7cbda4b2679d
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-08-23 15:44:05 +0600

    No satetement while loop

---
+++ WhileLoopNotStatement.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file WhileLoopNotStatement.cpp
+ * @author your name (you@domain.com)
+ * @brief No satetement while loop
+ * @version 0.1
+ * @date 2022-08-23
+ * @since TuesDay; 03:33 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+
+#include  <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    /* code */
+    system("cls");
+    int x, a, b;
+    x = 10, a = 5, b = 15;
+    cout<<"--x\t--a\t--b"<<endl;
+    printf("--------------------\n");
+    while (--a, --b, --x);
+    cout<<x<<"\t"<<a<<"\t"<<b<<endl;
+
+    // system("dir");
+    // system("vol");
+    system("edit");
+    return 0;
+}
