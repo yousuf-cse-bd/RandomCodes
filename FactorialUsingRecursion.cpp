dd18721782ce9b3f907e192dbdb04e08a3eaa5ee
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-09-11 23:09:18 +0600

    Recursion best example

--- FactorialUsingRecursion.cpp
+++ FactorialUsingRecursion.cpp
@@ -1,18 +1,29 @@
+/**
+ * @file FactorialUsingRecursion.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Recursion best example
+ * @version 0.1
+ * @date 2022-09-11
+ * @since SunDay; 11:01 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
 #include <iostream>
 using namespace std;
 
-unsigned int factorial(unsigned int n)
-{
-    if(n == 1)
-    return 1;
-    else
-    return n*(factorial(n - 1));
-
+unsigned long long int factorial(unsigned short int n){
+    if(n == 0){
+        return 1;
+    }
+    else{
+        return n*(factorial(n - 1));
+    }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     /* code */
-    cout<<factorial(5)<<endl;
+    system("cls");
+    unsigned short int number = 10;
+    cout<<number<<"! Factorial = "<<factorial(number)<<endl;
     return 0;
 }
