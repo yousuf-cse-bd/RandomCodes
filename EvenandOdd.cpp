dd0c80bca72846dcea7c715bbf87c14da13ef91a
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-09-11 22:48:00 +0600

    Sorting elements by odd and even separately

--- EvenandOdd.cpp
+++ EvenandOdd.cpp
@@ -1,51 +1,75 @@
-/****
- * @Author: Muhammad Yousuf Ali
- * @Since: 31 July 2019
+/**
+ * @file EvenandOdd.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Sorting elements by odd and even separately
+ * @version 0.1
+ * @date 2019-07-31
+ * @since WednesDay; 10:10 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
  */
 
 #include <iostream>
 using namespace std;
-
-void evenSort(int A[],int n)
-{
-    int i,j,item;
-    for ( i = 0; i < n; i++)
-    {
-        cout<<i<<" = "<<A[i]<<endl;
+/*Impelementing optimal bubble sort ascending oorder*/
+void bubleSort(int *A, unsigned int size){
+    bool swapStatus = true;
+    for(unsigned int i = 0; i < size - 1 && swapStatus == true; i++){
+        swapStatus = false;
+        for(unsigned int j = 0; j < size - (i+1); j++){
+            if(*(A+j) > *(A+j+1)){
+                swap(*(A+j), *(A+j+1));
+                swapStatus = true;
+            }
+        }
     }
-        
 }
-void oddSort(int n, int size)
-{
-   int i,j,item; 
+void evenSort(int *even, unsigned int size){
+    bubleSort(even, size);
+    cout<<"Even Elements Below:"<<endl;
+    for(unsigned int i = 0; i<size; i++){
+        cout<<*(even+i)<<" ";
+    }
+    cout<<endl;
+}
+void oddSort(int *odd, int size){ 
+    bubleSort(odd, size);
+    cout<<"Odd Elements Below:"<<endl;
+    for(int i = size - 1; i>=0; i--){
+        cout<<*(odd+i)<<" ";
+    }
+    cout<<endl;
 }
 
-int main(int argc, char const *argv[])
-{
-    int test, increment, num, size = 0;
-
+int main(int argc, char const *argv[]){
+    system("cls");
+    int testCase, increment, element;
+    unsigned int i = 0, j = 0;
     cout<<"Enter Nth Here: ";
-    cin>>test;
+    cin>>testCase;
+
+    int *even = (int*)malloc(sizeof(int));
+    int *odd = (int*)malloc(sizeof(int));
+
     increment = 1;
-    int A[test];
-    while(increment<=test)
-    {
-        cin>>num;
-        if(num % 2 == 0)
-        {
-            
-            A[increment] = num;
-            cout<<"E["<<increment<<"] = "<<A[increment]<<"\n";
-            size ++;
+    while(increment <= testCase){
+        cout<<"Enter Element Here: ";
+        cin>>element;
+        if(element % 2 == 0){
+            *(even+i) = element;
+            cout<<"Even: ["<<increment<<"] = "<<*(even+i)<<endl;
+            i++;
         }
-        else
-        {
-            A[increment] = num;
-            cout<<"O["<<increment<<"] = "<<A[increment]<<"\n";
+        else{
+            *(odd+j) = element;
+            cout<<"Odd: ["<<increment<<"] = "<<*(odd+j)<<endl;
+            j++;
         }
-        
         increment++;
     }
-
+    evenSort(even, i);
+    oddSort(odd, j);
+    free(even);
+    free(odd);
     return 0;
 }
