4b48bc272e7c8af35f9515545b737eb27d9dd7e5
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-09-23 23:03:38 +0600

    Factorial using recursion function

--- RecursionFnFactorial.cpp
+++ RecursionFnFactorial.cpp
@@ -1,29 +1,38 @@
+/**
+ * @file RecursionFnFactorial.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Factorial using recursion function
+ * @version 0.1
+ * @date 2022-09-23
+ * @since FriDay; 11:00 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
 #include <iostream>
 using namespace std;
 
-long long int factorial(long long int n)
+unsigned long long int factorial(unsigned short int n)
 {
-    if(n == 1)
-    {
+    if(n <= 1){
         return 1;
     }
-    else
-    {
-        return n*factorial(n-1);
+    else{
+        return (n*factorial(n-1));
     }
-    
 }
 
-int main(void)
+int main(int argc, char const *argv[])
 {
-    long long int n, fct;
-
+    /* code */
+    system("cls");
+    cout<<"Time: "<<__TIME__<<endl;
+    cout<<"Date: "<<__DATE__<<endl;
+    unsigned short int n;
     cout<<"Enter Nth Value Here: ";
     cin>>n;
 
-    fct = factorial(n);
+    unsigned long long int factValue = factorial(n);
 
-   cout<<n<<"  Factorial : "<<fct<<endl;
-
-   return 0;
+    cout<<n<<"!  Factorial : "<<factValue<<endl;
+    return 0;
 }
