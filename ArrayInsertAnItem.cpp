32674c95a0431439505d03a14fb0b1376d645a2a
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-01-23 23:14:45 +0600

    Insert an element of existing array with array notation

--- ArrayInsertAnItem.cpp
+++ ArrayInsertAnItem.cpp
@@ -1,16 +1,24 @@
+/**
+ * @file ArrayInsertAnItem.cpp
+ * @author your name (you@domain.com)
+ * @brief Insert an element of existing array with array notation
+ * @version 0.1
+ * @date 2023-01-23
+ * @since MonDay; 11:13 PM
+ * @copyright Copyright (c) 2023
+ * @institute: Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     /* code */
     system("cls");
     int n, newItem, i;
     cout<<"Enter Nth Value Here: ";
     cin>>n;
     int A[n+1];
-    for(i = 0; i<n; i++)
-    {
+    for(i = 0; i<n; i++){
         cout<<"A["<<i<<"]: ";
         cin>>A[i];
     }
@@ -18,29 +26,23 @@ int main(int argc, char const *argv[])
     int position;
     cout<<"Enter the Position where Inserting an Item: ";
     cin>>position;
-    if(position > n)
-    {
+    if(position > n){
         cout<<"Overflow the Array Sized...!"<<endl;
         exit(0);
     }
-    else
-    {
-        for(i = n+1; i>=position; i--)
-        {
+    else{
+        for(i = n; i>=position; i--){
             A[i] = A[i-1];
         }
-        A[i+1] = A[i];
         cout<<"Enter the new Item Here: ";
         cin>>newItem;
-        A[position] = newItem;
+        A[position] = newItem; 
 
         // Out Put:
         int N = sizeof(A) / sizeof(int);
-        for(i = 0; i<N; i++)
-        {
+        for(i = 0; i<N; i++){
             cout<<"A["<<i<<"]: "<<A[i]<<endl;
         }
-        
     }
     
     return 0;
