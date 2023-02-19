cb81d0a9186f4c65f2556ff628b56854d3bf9925
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-02-20 00:08:57 +0600

    Sum of nth number using recursion method

--- SumRecursion.cpp
+++ SumRecursion.cpp
@@ -1,17 +1,32 @@
+/**
+ * @file SumRecursion.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Sum of nth number using recursion method
+ * @version 0.1
+ * @date 2023-02-20
+ * @since SunDay; 12:08 AM
+ * @copyright Copyright (c) 2023
+ * @institute: Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
 #include <iostream>
 using namespace std;
 
-unsigned int recursion(unsigned int n)
-{
-    if(n == 1)
-    return 1;
-    else
-    return n+recursion(n - 1 );
+constexpr size_t sumRecursion(const int &n){
+    if(n <= 1){
+        return n;
+    }
+    else{
+        return (n+sumRecursion(n - 1));
+    }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     /* code */
-    cout<<"Sum of: "<<recursion(5)<<endl;
+    system("cls");
+    int n;
+    cout<<"Enter a positive number here: ";
+    cin>>n;
+
+    cout<<"\tThe sum of "<<abs(n)<<": "<<sumRecursion(abs(n))<<endl;
     return 0;
 }
